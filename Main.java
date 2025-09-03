aa2313ec5bba1bc5ca41e9df5f9caf33a97da9e6
Christian Jardim <christianjardim.aluno@unipampa.edu.br>
2025-09-03 12:04:44 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,20 +1,5 @@
-package main;
-
-import javax.swing.JFrame;
-
 public class Main {
-
 	public static void main(String[] args) {
-		JFrame windown = new JFrame ();
-		windown.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		windown.setResizable(true);
-		windown.setTitle("2D Adventure");
-		
-		GamePanel panel = new GamePanel ();
-		windown.add(panel);
-		windown.pack();
-		
-		windown.setLocationRelativeTo(null);
-		windown.setVisible(true);
 	}
+
 }
