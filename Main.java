c6b863956692d4038c59aa5433c27c33bef2b05c
Christian Jardim <christianjardim.aluno@unipampa.edu.br>
2025-09-03 16:01:05 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,64 @@
+import java.util.Scanner;
+
+enum Descricao {
+	ARROZ, FEIJAO, FARINHA, LEITE;
+}
+
+enum TipoPagamento {
+	DINHEIRO, CHEQUE, CARTAO;
+}
+
+class Produto {
+	private Descricao descricao;
+	private int quantidadeEstoque;
+}
+
+class Item {
+	private int quantidade;
+}
+
+class Pedido {
+	private TipoPagamento pagamento;
+}
+
+class Cliente {
+	private String nome;
+	private String CPF;
+	
+	public Cliente {
+	    nome = n;
+	    CPF = cpf;
+	}
+}
+
 public class Main {
 	public static void main(String[] args) {
+	    int op;
+	    Cliente c; 
+	    Produto p;
+	    Item q;
+	    Scanner input = new Scanner(System.in);
+	    
+		do {
+		    op = menu(input);
+		    switch(op) {
+		        case 1:
+		            
+		    }
+
+		} while (op != 0);
 	}
 
+	public static int menu(Scanner input) {
+		int op;
+		
+		do {
+			System.out.println(" 1) Novo pedido\n 2) Realizar pagamento\n 0) Sair da aplicacao");
+			System.out.print(" Escolha uma das opcoes: ");
+			op = input.nextInt();
+		} while(op<0 || op>3);
+		
+		input.nextLine();
+		return op;
+	}
 }
