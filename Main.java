142b4508c068a87d901fc82526d2fb60cc4f3d48
Christian Jardim <christianjardim.aluno@unipampa.edu.br>
2025-09-04 18:10:16 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -25,40 +25,54 @@ class Cliente {
 	private String nome;
 	private String CPF;
 	
-	public Cliente {
-	    nome = n;
-	    CPF = cpf;
+	public Cliente(String n, String cpf) {
+		nome = n;
+		CPF = cpf;
+	}
+	
+	public void seApressentar (){
+	    System.out.println("\n Meu nome e: " + nome + " \n" + " Meu CPF e: " + CPF);
 	}
 }
 
 public class Main {
 	public static void main(String[] args) {
-	    int op;
-	    Cliente c; 
-	    Produto p;
-	    Item q;
-	    Scanner input = new Scanner(System.in);
-	    
+		int op;
+		Cliente c;
+		Produto p;
+		Item q;
+		Scanner input = new Scanner(System.in);
+
 		do {
-		    op = menu(input);
-		    switch(op) {
-		        case 1:
-		            
-		    }
+			op = menu(input);
+			switch(op) {
+			case 1:
+				c = recebeCliente(input);
+				c.seApressentar();
+				break;
+			}
 
 		} while (op != 0);
 	}
 
 	public static int menu(Scanner input) {
 		int op;
-		
+
 		do {
-			System.out.println(" 1) Novo pedido\n 2) Realizar pagamento\n 0) Sair da aplicacao");
+			System.out.println("\n 1) Novo pedido\n 2) Realizar pagamento\n 0) Sair da aplicacao");
 			System.out.print(" Escolha uma das opcoes: ");
 			op = input.nextInt();
 		} while(op<0 || op>3);
-		
+
 		input.nextLine();
 		return op;
+	}
+
+	public static Cliente recebeCliente(Scanner input) {
+		System.out.print(" \nCliente:\nDigite o nome do Cliente: ");
+		String nome = input.nextLine();
+		System.out.print("Digite o CPF do cliente: ");
+		String cpf = input.nextLine();
+		return (new Cliente(nome, cpf));
 	}
 }
