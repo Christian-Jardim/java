9c804cd1b8fb56b3e58017a1bfe3b7c68af086f0
Christian Jardim <christianjardim.aluno@unipampa.edu.br>
2025-09-10 12:06:29 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,7 +10,28 @@ enum TipoPagamento {
 
 class Produto {
 	private Descricao descricao;
+	private double preco;
 	private int quantidadeEstoque;
+
+	public Produto(int t, int v, int q) {
+		switch(t) {
+		case 1:
+			descricao = Descricao.ARROZ;
+			break;
+		case 2:
+			descricao = Descricao.FEIJAO;
+			break;
+		case 3:
+			descricao = Descricao.FARINHA;
+			break;
+		case 4:
+			descricao = Descricao.LEITE;
+			break;
+		}
+
+		preco = v;
+		quantidadeEstoque = q;
+	}
 }
 
 class Item {
@@ -24,15 +45,14 @@ class Pedido {
 class Cliente {
 	private String nome;
 	private String CPF;
-	
+
 	public Cliente(String n, String cpf) {
 		nome = n;
 		CPF = cpf;
 	}
-	
-	public String seApresentar (){
 
-	    return "\n Cliente -> Nome: " + nome + "\n\t    CPF: " + CPF;
+	public String seApresentar () {
+		return "\n Cliente -> Nome: " + nome + "\n\t    CPF: " + CPF;
 	}
 }
 
@@ -43,15 +63,16 @@ public class Main {
 		Produto p;
 		Item q;
 		Scanner input = new Scanner(System.in);
-
+        
 		do {
 			op = menu(input);
 			switch(op) {
 			case 1:
 				c = recebeCliente(input);
 				System.out.println(c.seApresentar());
-				p = recebeProduto(input);
 				break;
+			case 3:
+			     p = recebeProduto(input);
 			}
 		} while (op != 0);
 	}
@@ -60,10 +81,10 @@ public class Main {
 		int op;
 
 		do {
-			System.out.println("\n 1) Novo pedido\n 2) Realizar pagamento\n 0) Sair da aplicacao");
+			System.out.println("\n 1) Novo pedido\n 2) Realizar pagamento\n 3) Cadastrar um produto\n 0) Sair da aplicacao");
 			System.out.print(" Escolha uma das opcoes: ");
 			op = input.nextInt();
-		} while(op<0 || op>3);
+		} while(op<0 || op>4);
 
 		input.nextLine();
 		return op;
@@ -76,12 +97,14 @@ public class Main {
 		String cpf = input.nextLine();
 		return (new Cliente(nome, cpf));
 	}
-	
-	public static Cliente recebeProduto(Scanner input) {
-		System.out.print("\n Cliente:\n Digite o nome do Cliente: ");
-		String nome = input.nextLine();
-		System.out.print(" Digite o CPF do cliente: ");
-		String cpf = input.nextLine();
-		return (new Cliente(nome, cpf));
+
+	public static Produto recebeProduto(Scanner input) {
+		System.out.print(" Digite o valor do produto: ");
+		int valor = input.nextInt();
+		System.out.print("\n Produto:\n Digite o numro do produto (1-arroz 2-feijao 3-farinha 4-leite): ");
+		int tipo = input.nextInt();
+		System.out.print(" Digite a quantidade do produto: ");
+		int quant = input.nextInt();
+		return (new Produto(tipo, valor, quant));
 	}
 }
