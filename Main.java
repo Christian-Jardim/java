fe49519cef47b6f560bf7ea6f792d8ff88ac76da
Christian Jardim <christianjardim.aluno@unipampa.edu.br>
2025-09-11 16:17:37 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -16,22 +16,18 @@ class Produto {
 
 	public Produto(int t, int v, int q) {
 		switch(t) {
-		case 1:
-			descricao = Descricao.ARROZ;
-			break;
-		case 2:
-			descricao = Descricao.FEIJAO;
-			break;
-		case 3:
-			descricao = Descricao.FARINHA;
-			break;
-		case 4:
-			descricao = Descricao.LEITE;
-			break;
+		case 1: descricao = Descricao.ARROZ; break;
+		case 2: descricao = Descricao.FEIJAO; break;
+		case 3: descricao = Descricao.FARINHA; break;
+		case 4: descricao = Descricao.LEITE; break;
 		}
 
 		preco = v;
 		quantidadeEstoque = q;
+	}
+	
+	public String mostraProduto() {
+		return "\n Item -> Tipo: " + descricao + "\n\t Quantidade: " + quantidade;
 	}
 }
 
@@ -43,41 +39,45 @@ class Item {
 		quantidade = q;
 
 		switch(t) {
-		case 1:
-			descricao = Descricao.ARROZ;
-			break;
-		case 2:
-			descricao = Descricao.FEIJAO;
-			break;
-		case 3:
-			descricao = Descricao.FARINHA;
-			break;
-		case 4:
-			descricao = Descricao.LEITE;
-			break;
+		case 1: descricao = Descricao.ARROZ; break;
+		case 2: descricao = Descricao.FEIJAO; break;
+		case 3: descricao = Descricao.FARINHA; break;
+		case 4: descricao = Descricao.LEITE; break;
 		}
+	}
+
+	public String mostraItem() {
+		return "\n Item -> Tipo: " + descricao + "\n\t Quantidade: " + quantidade;
 	}
 }
 
 class Pedido {
 	private TipoPagamento pagamento;
-	private List<Item> itens;
+	private LinkedList<Item> itens;
+
+	public Pedido() {
+		itens = new LinkedList<Item>();
+	}
 
 	public Pedido(int t) {
-
 		switch(t) {
-		case 1:
-			pagamento = TipoPagamento.DINHEIRO;
-			break;
-		case 2:
-			pagamento = TipoPagamento.CARTAO;
-			break;
-		case 3:
-			pagamento = TipoPagamento.CHEQUE;
-			break;
+		case 1: pagamento = TipoPagamento.DINHEIRO; break;
+		case 2: pagamento = TipoPagamento.CARTAO; break;
+		case 3: pagamento = TipoPagamento.CHEQUE; break;
+		}
+	}
+
+	public void preenche(Item item) {
+		itens.add(item);
+	}
+
+	public void mostraPedido() {
+		for (Item item : itens) {
+			System.out.println(item.mostraItem());
 		}
 	}
 }
+
 class Cliente {
 	private String nome;
 	private String CPF;
@@ -94,41 +94,52 @@ class Cliente {
 
 public class Main {
 	public static void main(String[] args) {
-		int op,num,i;
-		i = 0;
-		Cliente c;
-		Produto p;
-		Item q;
+		int op1,op2;
+		Cliente cliente;
+		Produto produto;
+		Item item;
 		Scanner input = new Scanner(System.in);
+		Pedido pedido = new Pedido();
 		LinkedList<Produto> estoque = new LinkedList<Produto>();
-		LinkedList<Item> pedido = new LinkedList<Item>();
 
 		do {
-			op = menu(input);
-			switch(op) {
+			op1 = menu1(input);
+			switch(op1) {
 			case 1:
-				c = recebeCliente(input);
-				System.out.println(c.seApresentar());
-
+				cliente = recebeCliente(input);
+				System.out.println(cliente.seApresentar());
+				recebePedido(input,pedido);
+				pedido.mostraPedido();
 				break;
 			case 2:
-				System.out.print(" Informe o tipo de pagamento: ");
-				num = input.nextInt();
-				Pedido pedi = new Pedido(num);
-			case 3:
-				p = recebeProduto(input);
-				estoque.add(p);
-			}
-		} while (op != 0);
 
-		for (Produto produto : estoque) {
-			System.out.println(produto.descricao);
-			System.out.println(produto.preco);
-			System.out.println(produto.quantidadeEstoque);
+			case 3:
+				produto = recebeProduto(input);
+				estoque.add(produto);
+			}
+		} while (op1 != 0);
+
+		for (Produto p : estoque) {
+			System.out.println(p.descricao);
+			System.out.println(p.preco);
+			System.out.println(p.quantidadeEstoque);
 		}
 	}
 
-	public static int menu(Scanner input) {
+	public static int menu1(Scanner input) {
+		int op;
+
+		do {
+			System.out.println("\n 1) Novo pedido\n 2) Realizar pagamento\n 3) Cadastrar um produto\n 0) Sair da aplicacao");
+			System.out.print(" Escolha uma das opcoes: ");
+			op = input.nextInt();
+		} while(op<0 || op>4);
+
+		input.nextLine();
+		return op;
+	}
+
+	public static int menu2(Scanner input) {
 		int op;
 
 		do {
@@ -159,26 +170,24 @@ public class Main {
 		return (new Produto(tipo, valor, quant));
 	}
 
-	public static Produto informaProduto(Scanner inpunt) {
-	    int i,num;
-	    Item item;
-	    i = num = 0;
-		System.out.print(" Informe a quantidade de tipos de itens do pedido: ");
-		num = input.nextInt();
+	public static void recebePedido(Scanner input, Pedido pedido) {
+		int i,x;
+		i = x = 0;
 
-		while(i<num) {
-			System.out.print(" Digite o numero do produto (1-arroz 2-feijao 3-farinha 4-leite): ");
+		/*System.out.print(" Informe o modo de pagamento: ");
+		x = input.nextInt();
+		Pedido pedido = new Pedido(x);*/
+
+		System.out.print("\n Informe a quantidade de itens do pedido: ");
+		x = input.nextInt();
+
+		while(i<x) {
+			System.out.print("\n Digite o codigo do item (" + (i + 1) + ") (1-arroz 2-feijao 3-farinha 4-leite): ");
 			int tipo = input.nextInt();
 			System.out.print(" Digite a quantidade dele: ");
-			int quant = input.nextInt();
-			item = new Item(quant, tipo);
-			pedido.add(item);
+			int quantidade = input.nextInt();
+			pedido.preenche(new Item(quantidade, tipo));
 			i++;
-		}
-
-		for (Item item : pedido) {
-			System.out.println(item.descricao);
-			System.out.println(item.quantidade);
 		}
 	}
 }
